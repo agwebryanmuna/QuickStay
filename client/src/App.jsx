ac5e76f5e181705bb135b352a3eae6741db5eb96
agwebryanmuna <agwebryan1@gmail.com>
2025-05-28 23:47:57 +0100

    Created Hero page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,19 @@
-export const App = () => {
-  return <div>App</div>;
+import { Route, Routes, useLocation } from "react-router";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const App = () => {
+  const isOwnerPath = useLocation().pathname.includes("/owner");
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
+
+export default App;
